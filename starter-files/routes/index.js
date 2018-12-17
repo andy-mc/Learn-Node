737cadf550baf2ba9c26d7b3e81499cf5b1ffc11
Andrés Muñoz <asmz5488@gmail.com>
2018-12-17 04:13:12 -0500

    working wth pug

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  
+  res.render('hello', {
+    title: 'Andy is growing',
+    name: req.query.name,
+    dog: req.query.dog
+  });
 });
+
+router.get('/reverse/:word', (req, res) => {
+  var word = req.params.word;
+  var reverseWord = reverseString(word);
+  res.send(reverseWord);
+});
+
+function reverseString(word) {
+  return [...word].reverse().join('');
+}
 
 module.exports = router;
