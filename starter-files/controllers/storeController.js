a25193b28c2bff574e111b6aedde1dac5fc3aa66
Andrés Muñoz <asmz5488@gmail.com>
2018-12-18 23:35:02 -0500

    catch erros

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.locals.title = 'andyPandy';
   res.render('index');
@@ -9,6 +12,10 @@ exports.addStore = (req, res) => {
   res.render('editStore', {title: '🏪 Add Store'});
 }
 
-exports.createStore = (req, res) => {
-  res.json(req.body)
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  console.log('Saved Succesfully !!')
+
+  res.redirect('/')
 }
