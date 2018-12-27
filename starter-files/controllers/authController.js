cf6544d251372b779e2c1b5a364d7db954e3932d
Andrés Muñoz <asmz5488@gmail.com>
2018-12-27 00:36:37 -0500

    show add screen just to loggen in users

--- starter-files/controllers/authController.js
+++ starter-files/controllers/authController.js
@@ -12,3 +12,13 @@ exports.logout = (req, res) => {
   req.flash('success', 'You are now logged out !! 🙂');
   res.redirect('/');
 };
+
+exports.isLoggedIn = (req, res, next) => {
+  // first check if the user is authenticated
+  if (req.isAuthenticated()) {
+    next();
+    return;
+  }
+  req.flash('error', 'Ooops you must be logged in to do that !!');
+  res.redirect('/login');
+};
