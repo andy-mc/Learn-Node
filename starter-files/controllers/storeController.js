b79d3a4006f7425b2576569aed56d48d7d5135ba
Andrés Muñoz <asmz5488@gmail.com>
2018-12-29 23:36:41 -0500

    endpoint for search

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -134,3 +134,18 @@ exports.getStoresByTag = async (req, res) => {
     stores
   });
 };
+
+exports.searchStores = async (req, res) => {
+  const stores = await Store.find(
+    {
+      $text: {
+        $search: req.query.q
+      }
+    },
+    { score: { $meta: 'textScore' } }
+  )
+    .sort({ score: { $meta: 'textScore' } })
+    .limit(5);
+
+  res.json(stores);
+};
